faf54ea04121f57e59b597b97cf07faa7ef0b51f
Ben <benbeltrao@gmail.com>
2025-06-27 11:44:21 -0300

    ajustando o layout

--- utils/marcadores.py
+++ utils/marcadores.py
@@ -6,5 +6,5 @@ texto = "#fffddf"
 textoSideBar = background
 
 def divisor():
-    return st.markdown("<hr style='border:1px solid #fffddf;'>", unsafe_allow_html=True)
+    return st.markdown("<hr style='border:1px solid #002b36;'>", unsafe_allow_html=True)
 
